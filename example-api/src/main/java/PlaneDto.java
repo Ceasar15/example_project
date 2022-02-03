9de5f25ceee761bce89ceb62d7a32971fd136ffd
Ceasar15 <ceasarkwadwo@gmail.com>
2022-02-03 14:23:11 +0000

    more config form pom and gitignore

--- example-api/src/main/java/PlaneDto.java
+++ example-api/src/main/java/PlaneDto.java
@@ -1,2 +1,18 @@
 public class PlaneDto {
+
+    String name;
+    int engineCounters;
+
+    public PlaneDto(String name, int engineCounters) {
+        this.name = name;
+        this.engineCounters = engineCounters;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEngineCounters() {
+        return engineCounters;
+    }
 }
